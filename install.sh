e1684f97cb30f1c09cc00299d88d5b591c3070bf
Kevin Kramer <kekram@physik.uzh.ch>
2017-03-17 16:11:52 +0100

    Made sure that install always creates backups of files to be overwritten

--- install.sh
+++ install.sh
@@ -8,20 +8,27 @@ function install {
     cp -r $1 $2
 }
 
+function backup {
+    echo "Creating backup of $1 in $1_bk"
+    cp -r $1 $1_bk
+}
+
 # Get destinations and locations from a routefile
 FILES=($(cat $ROUTEFILE))
 L=${#FILES[@]}
 
 i="0"
 
-# Update all files specified in the ROUTEFILE
+# Install all files specified in the ROUTEFILE
 while [ $i -lt $L ]; do
+    backup $HOME/${FILES[i]} 
     install ${FILES[i+1]} $HOME/${FILES[i]}
     i=$[$i+2]
 done
 
 # Special case for  .vim
 cp -r vim/ .vim/
+backup $HOME/.vim
 install .vim/ $HOME/
 rm -rf .vim/
 
