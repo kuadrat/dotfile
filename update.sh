80321991fb6893710da0261c37b1b58ed2303760
Kevin Kramer <kramer@phys.ethz.ch>
2017-03-17 15:21:23 +0100

    Improved updating procedure

--- update.sh
+++ update.sh
@@ -2,29 +2,26 @@
 # Get changes in config files by simply copying them all here.
 # Differences can then be checked with git diff
 
+ROUTEFILE=files.txt
+
 function update {
     echo "Copying $1 to $2"
     cp -r $1 $2
 }
 
-# .vim
+FILES=($(cat $ROUTEFILE))
+L=${#FILES[@]}
+
+i="0"
+
+while [ $i -lt $L ]; do
+    update $HOME/${FILES[i]} ${FILES[i+1]}
+    i=$[$i+2]
+done
+
+#  Special case for .vim
 cp -r $HOME/.vim/ $HOME/vim/
 update $HOME/vim/ . 
 rm -rf $HOME/vim/
-
-update $HOME/.vimrc ./vim
-
-# .bashrc
-update $HOME/.bashrc ./bashrc
-
-# .toprc
-update $HOME/.toprc ./toprc
-
-# .screenrc
-update $HOME/.screenrc ./screenrc
-
-# matplotlibrc
-update $HOME/.config/matplotlib/matplotlibrc ./matplotlibrc
-
 
 echo "Config files updated."
